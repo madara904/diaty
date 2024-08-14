dbcb01ad684e03df21c06026ebb896826896b528
madara904 <160205719+madara904@users.noreply.github.com>
2024-08-14 10:50:45 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
   return (
     <div className="mt-5 text-center">
       <h1 className="text-7xl md:text-8xl relative text-balance font-serif"><span>Discover </span>   
-      <span className="before:block before:absolute before:-inset-1 before:translate-x-4 before:bg-gray-600 before:w-40 relative inline-block z-0">
+      <span className="before:block before:absolute before:-inset-1 before:translate-x-4 before:bg-gray-800 before:w-40 relative inline-block z-0">
     <span className={cn("relative text-primary", inter.className)}> diaty</span>
     </span> and start your healthy journey today!
     </h1>
