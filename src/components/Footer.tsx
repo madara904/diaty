b31ee45ac11328eda3d9c99785ad29e7ee1a9df7
madara904 <160205719+madara904@users.noreply.github.com>
2024-08-14 11:35:28 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import Link from "next/link"
 const Footer = () => {
   return (
     <>
-    <footer className="flex flex-col mt-15 sm:-mt-14">
-    <hr className="dark:border-gray-700 w-full self-center"/>
+    <footer className="flex flex-col mt-24 sm:-mt-14">
+    <hr className="dark:border-gray-500 w-full self-center"/>
       <div className="flex justify-center text-xs font-bold h-12 items-center">
       <Link href={"https://github.com/madara904"}><Github size={30} color="" strokeWidth="1" fill="gray" className="mt-1 hover:opacity-80 cursor-pointer"/></Link>
       <span></span>
