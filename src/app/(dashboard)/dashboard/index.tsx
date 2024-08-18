278fdb57dd4258138a609a8d82c5d7798cb14097
madara904 <gundogduberay@gmail.com>
2024-08-18 23:16:08 +0200

    1111

--- src/app/(dashboard)/dashboard/index.tsx
+++ src/app/(dashboard)/dashboard/index.tsx
@@ -1,10 +1,4 @@
-"use client"
-
-import { useState } from "react";
 import { Overview } from "./components/Overview";
-import Sidebar from "./components/Sidebar";
-import Profile from "./profile/page";
-import { usePathname } from "next/navigation";
 
 const DashboardLayout: React.FC = () => {
 
