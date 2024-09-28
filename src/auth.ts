f640a9b1d1b0a032bb32e3b7bcd077b5abb054b3
madara904 <gundogduberay@gmail.com>
2024-09-28 13:57:06 +0200

    ok

--- src/auth.ts
+++ src/auth.ts
@@ -20,7 +20,7 @@ export const providerMap = providers.map((provider) => {
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers,
-  secret: process.env.SECRET,
+  secret: process.env.AUTH_SECRET,
   pages: {
     signIn: "/sign-in",
   },
