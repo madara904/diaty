5f0a13fba191c3841bf9e031dc8bd4bdc46c70bc
madara904 <gundogduberay@gmail.com>
2024-10-03 21:26:20 +0200

    Runtime edge fix for middleware amk

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "./lib/prisma"
 import type { Provider } from "next-auth/providers"
+import authConfig from "./auth.config"
 
 
 const providers: Provider[] = [
@@ -19,17 +20,9 @@ export const providerMap = providers.map((provider) => {
 })
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [Google({
-    clientId: process.env.AUTH_GOOGLE_ID,
-    clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    authorization: {
-      params: {
-        scope: "openid email profile",
-      },
-    },
-  })],
   adapter: PrismaAdapter(prisma),
   pages: {
     signIn: "/sign-in",
   },
+  ...authConfig,
 })
